5518417eebd3d1f36ba3b3f9c9b7fcbd88aa3d17
o0th <o0th@pm.me>
2022-09-07 11:40:17 +0200

    :heavy_plus_sign: Mofiqul/dracula.nvim

---
+++ init.lua
@@ -0,0 +1,30 @@
+local config = {
+  colorscheme = "dracula",
+
+  plugins = {
+    init = {
+      { "Mofiqul/dracula.nvim" }
+    }
+  },
+
+  options = {
+    opt = {
+      relativenumber = false
+    }
+  },
+
+  header = {
+    " ",
+    " ",
+    " ",
+    " ",
+    "o0th",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+  },
+}
+
+return config
